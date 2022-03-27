a1afb15392ed3bc0db31f8a0f9ccd243e2bb07c6
Mauro Correia <mauroac1@outlook.com>
2022-03-27 19:09:44 -0300

    Multiples of 3 and 5.

--- code-wars/Multiples of 3 or 5.js
+++ code-wars/Multiples of 3 or 5.js
@@ -1,3 +1,6 @@
+/*               6 KYU || Multiples of 3 or 5               */
+
+
 /*               Instructions               */
 
 /* If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -20,4 +23,3 @@ function solution(...number){
     
     return totalSum;
   }
-  
