dbb7f257eae1a6ec6b0330d0057bab3da04e53c6
Mauro Correia <mauroac1@outlook.com>
2022-03-27 19:06:30 -0300

    Day 1 of code-wars.

--- code-wars/Multiples of 3 or 5.js
+++ code-wars/Multiples of 3 or 5.js
@@ -1,4 +1,5 @@
 /*               Instructions               */
+
 /* If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally,
@@ -6,7 +7,8 @@ if the number is negative, return 0 (for languages that do have them).
 
 Note: If the number is a multiple of both 3 and 5, only count it once. */
 
-/* Solution */
+
+/*               Solution               */
 
 function solution(...number){
 
@@ -18,3 +20,4 @@ function solution(...number){
     
     return totalSum;
   }
+  
