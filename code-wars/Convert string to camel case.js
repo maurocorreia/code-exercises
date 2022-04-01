b532a467c2e1646a155f21e9554bc763af55ee79
Mauro Correia <mauroac1@outlook.com>
2022-04-01 12:43:49 -0300

    Convert string to camel case.

--- code-wars/Convert string to camel case.js
+++ code-wars/Convert string to camel case.js
@@ -12,6 +12,6 @@ also often referred to as Pascal case). */
 /*               Solution               */
 
 function toCamelCase(str){
-    
+
     return str.split(/-|_/).map((word, index) => (index > 0) ? word.charAt(0).toUpperCase() + word.slice(1) : word).join('');
-  }
+}
