8a109b836c89879439e6454b007b38c66fe5fba7
Mauro Correia <mauroac1@outlook.com>
2022-04-03 00:13:14 -0300

    Moving Zeros To The End.

---
+++ code-wars/Moving Zeros To The End.js
@@ -0,0 +1,16 @@
+/*               5 KYU || Moving Zeros To The End              */
+
+
+/*               Instructions               */
+
+/* Write an algorithm that takes an array and moves all of the zeros
+to the end, preserving the order of the other elements.
+
+    [false,1,0,1,2,0,1,3,"a"] >>>> [false,1,1,2,1,3,"a",0,0]    */
+
+
+/*               Solution               */
+
+function moveZeros(arr) {
+    return [...arr.filter(e => e !== 0), ...arr.filter(e => e === 0)];
+}
